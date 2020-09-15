6f72e8f2a2bac64e880052b3f98d588f00b15fd7
Warren Niu <warrenniu@gmail.com>
2020-09-15 16:42:12 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -137,7 +137,7 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-		programmer_hash[:dennis_ritchie][:languages].push(Assembly")
+		programmer_hash[:dennis_ritchie][:languages].push("Assembly")
 
 		programmer_hash
 
